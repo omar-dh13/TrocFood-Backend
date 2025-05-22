984447db0f393f9c305b0fc716f414d59ad41195
Florian <florian.marie92@gmail.com>
2025-05-22 10:39:39 +0200

    modifs de location avec l'index 2dsphere pour MongoDB sur le models dons.js / Géré avec des coordonnées latitude longitude

--- models/dons.js
+++ models/dons.js
@@ -3,13 +3,18 @@ const mongoose = require('mongoose');
 const donSchema = new mongoose.Schema({
   title: String,
   description: String, 
-  image: String, // URL de l’image
-  location: String, 
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'users' }, // Référence vers l’utilisateur
-  createdAt: { type: Date, default: Date.now }, // Date automatique
+  image: String,
+  location: {
+    type: { type: String, default: 'Point' },
+    coordinates: [Number], // [longitude, latitude]
+  },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+  createdAt: { type: Date, default: Date.now },
 });
 
-
+donSchema.index({ location: "2dsphere" }); 
+// L'index 2dsphere est utilisé dans MongoDB pour les données géospatiales du type "Point" en GeoJSON (comme des coordonnées de latitude et longitude).
+// ce sont des des coordonnées géographiques sur une sphère (la Terre).
 const Don = mongoose.model('dons', donSchema);
 
 module.exports = Don;
