49ed6282b71c17fd89125d56cac64d529e2ea16f
Margaux <margaux.courageux@gmail.com>
2025-05-22 18:12:56 +0200

    route post createprofile fonctionnelle sans adresse et le birthday

--- routes/users.js
+++ routes/users.js
@@ -13,6 +13,14 @@ router.post("/signup", (req, res) => {
     return;
   }
 
+  // vérification du format de l'email
+  const patternMail =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!patternMail.test(req.body.email)) {
+    res.json({ result: false, error: "Invalid email format" });
+    return;
+  }
+
   // Check if the user has not already been registered
   User.findOne({ email: req.body.email }).then((data) => {
     if (data === null) {
@@ -53,6 +61,79 @@ router.post("/signin", (req, res) => {
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
+});
+
+// POST pour ajouter les infos utilisateur de EditProfile
+router.post("/profile", (req, res) => {
+  const {
+    email,
+    userName,
+    firstName,
+    lastName,
+    phone,
+    /*birthday,
+    street,*/
+    token,
+  } = req.body;
+
+  //vérification des champs
+  if (
+    !checkBody(req.body, [
+      "email",
+      "userName",
+      "firstName",
+      "lastName",
+      "phone",
+      /*"street",
+      "birthday",*/
+    ])
+  ) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+
+  // vérification du format de l'email
+  const patternMail =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!patternMail.test(email)) {
+    res.json({ result: false, error: "Invalid email format" });
+    return;
+  }
+
+  //vérification du format du téléphone
+  const patternTel = /(0|(\\+33)|(0033))[1-9][0-9]{8}/;
+  if (!patternTel.test(phone)) {
+    res.json({ result: false, error: "Invalid phone number format" });
+    return;
+  }
+
+  // update des infos utilisateurs si token trouvé
+  User.findOne({ token: token }).then((data) => {
+    if (data) {
+      data.email !== email ? (data.email = email) : null;
+      data.userName = userName;
+      data.firstName = firstName;
+      data.lastName = lastName;
+      data.phone = phone;
+      // data.birthday = birthday;
+      // data.adress = {
+      //   street: street[0].properties.name,
+      //   postalCode: street[0].properties.postcode,
+      //   city: street[0].properties.city,
+      //   country: "France", // TODO : checker si on garde en brut ou si on ajoute un input pour le pays
+      //   location: {
+      //     type: street[0].geometry.type,
+      //     coordinates: street[0].geometry.coordinates,
+      //   },
+      // };
+
+      data.save().then((newDoc) => res.json({ result: true, user: newDoc }));
+
+      // réponse si token non trouvé
+    } else {
+      res.json({ result: false, error: "User not found" });
+    }
+  });
 });
 
 module.exports = router;
